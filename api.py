4fbaf5708ff3862850fbee92a432a3690a0bb1d6
99zql <166954461+99zql@users.noreply.github.com>
2026-08-27 15:38:58 -0300

    Create api.py

---
+++ api.py
@@ -0,0 +1,67 @@
+funcionarios = {
+    "Samuel": {
+        "id": 1,
+        "role": "Assistente Contábil",
+        "up": 0,
+        "down": 0
+    },
+
+    "Sophia": {
+        "id": 1,
+        "role": "Assistente Contábil",
+        "up": 0,
+        "down": 0
+    },
+
+    "Lucas": {
+        "id": 1,
+        "role": "Assistente Contábil",
+        "up": 0,
+        "down": 0
+    },
+
+    "Murillo": {
+        "id": 1,
+        "role": "Assistente Contábil",
+        "up": 0,
+        "down": 0
+    },
+
+    "Luis": {
+        "id": 1,
+        "role": "Assistente Contábil",
+        "up": 0,
+        "down": 0
+    },
+
+    "Giovanna": {
+        "id": 1,
+        "role": "Assistente Contábil",
+        "up": 0,
+        "down": 0
+    },
+
+    "Felipe": {
+        "id": 1,
+        "role": "Assistente Contábil",
+        "up": 0,
+        "down": 0
+    },
+
+    "Maria Luiza": {
+        "id": 1,
+        "role": "Assistente Contábil",
+        "up": 0,
+        "down": 0
+    },
+
+    "Gabriel": {
+        "id": 1,
+        "role": "Assistente Contábil",
+        "up": 0,
+        "down": 0
+    }
+}
+
+print(funcionarios)
+
